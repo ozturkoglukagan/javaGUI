c734af3ae73a2026af855c59c616bf7e577b4449
Kagan Ozturkoglu <ozturkoglukagan@gmail.com>
2021-09-26 23:48:16 +0300

    added a trial file and restarted for login screen

--- GUI.java
+++ GUI.java
@@ -5,30 +5,9 @@ import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener{
 
-    private int count=0;
-    private JLabel label;
-    private JFrame frame;
-    private JPanel panel;
-
     public GUI(){
 
-        frame = new JFrame();
-        JButton button = new JButton("Do you love me?");
-        button.addActionListener(this);
-        panel = new JPanel();
-        label = new JLabel("Love Counter: 0");
-
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(button);
-        panel.add(label);
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("My Leafy");
-        frame.pack();
-        frame.setVisible(true);
-
+    
         
     }
     
@@ -42,7 +21,6 @@ public class GUI implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Love Counter: "+count);        
+         
     }
 }
